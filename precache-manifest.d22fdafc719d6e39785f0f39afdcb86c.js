c5b433f5e082763c6cef09b56c0c696c1c02229c
koussay bilel <koussaybilel02@gmail.com>
2020-04-26 04:01:07 +0100

    Updates

---
+++ precache-manifest.d22fdafc719d6e39785f0f39afdcb86c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ea821ae3659ec4365640412abaccbdc",
+    "url": "/koussay/index.html"
+  },
+  {
+    "revision": "128585d3377e0c8133f4",
+    "url": "/koussay/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "e0a01a36ee5153f404f8",
+    "url": "/koussay/static/css/main.c55f0689.chunk.css"
+  },
+  {
+    "revision": "128585d3377e0c8133f4",
+    "url": "/koussay/static/js/2.fa494c61.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/koussay/static/js/2.fa494c61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0a01a36ee5153f404f8",
+    "url": "/koussay/static/js/main.725e814c.chunk.js"
+  },
+  {
+    "revision": "7d94eb9cd4ffc50c1721",
+    "url": "/koussay/static/js/runtime-main.63faf632.js"
+  }
+]);
