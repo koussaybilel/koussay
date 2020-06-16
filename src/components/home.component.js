6aaf45eed245fc94dd299861fcc2a63612a72563
koussay bilel <koussaybilel02@gmail.com>
2020-06-16 15:45:40 +0100

    Fix bad repo

---
+++ src/components/home.component.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+
+
+export default class Home extends Component {
+  constructor(props) {
+    super(props);
+
+   
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <header className="jumbotron">
+          <h3>koussay</h3>
+        </header>
+      </div>
+    );
+  }
+}
